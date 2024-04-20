79817ff49735562698701940e7f3516213fa8b04
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-04-20 16:18:52 -0400

    try once more inner imports from maps

--- VRBoilerplate.js
+++ VRBoilerplate.js
@@ -1,6 +1,6 @@
 //make and export scene
 
-//import * as THREE from 'three';
+import * as THREE from 'three';
 
   // import { BoxLineGeometry } from 'three/addons/geometries/BoxLineGeometry.js';
  //  const VRButton= await import( 'https://cdn.jsdelivr.net/npm/three@v0.163.0/examples/jsm/webxr/VRButton.js');
