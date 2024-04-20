e312c91ffa21ff1826cd48bfd9a61c6a0c990683
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-04-20 18:42:27 -0400

    remove test

--- MyndPalace.js
+++ MyndPalace.js
@@ -2,10 +2,6 @@
 
 import { init, animate } from 'vr/VRBoilerplate.js';
 
-let t= document.createElement('div');
-t.innerHTML= init + animate;
-document.body.appendChild(t);
-
 export function enter(){
   init();
   animate();
