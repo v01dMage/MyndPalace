796b119b6c58cedd9bfa3372cf31e6eb99da72de
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-04-20 17:49:17 -0400

    export test cont.

--- MyndPalace.js
+++ MyndPalace.js
@@ -1,9 +1,9 @@
 //Main entry to vr lab
 
-import 'vr/VRBoilerplate.js';
+import { testFn } from 'vr/VRBoilerplate.js';
 
 let t= document.createElement('div');
-t.innerHTML= 'mynd';
+t.innerHTML= testFn;
 document.body.appendChild(t);
 
 export function enter(){
