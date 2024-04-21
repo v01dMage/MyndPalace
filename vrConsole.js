b5db7c692e61d385bba31be367da6e328ece19b5
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-04-21 10:28:45 -0400

    pipeline++

--- vrConsole.js
+++ vrConsole.js
@@ -2,6 +2,14 @@
 import * as THREE from'three';
 import { vr } from 'vr/VRBoilerplate.js';
 
+let cFrame, cScreen, cPlane;
+let runner, testLight;
+
+vr.controller1.addEventListener( 'selectstart', onSelectStart );
+vr.controller1.addEventListener( 'selectend', onSelectEnd );
+vr.controller2.addEventListener( 'selectstart', onSelectStart );
+vr.controller2.addEventListener( 'selectend', onSelectEnd );
+
 function onSelectStart(){
     // Highlight runner
     if( runner.userData.isSelecting == true ) {
@@ -23,10 +31,22 @@ function onSelectEnd(){
        cScreen.add( cPlane );
 }
 
-function pipeline(){}
+function pipeline(){
+    const intersects = vr.raycaster.intersectObjects( [ runner ] );
+    if( intersects.length > 0 ) {
+        runner.userData.isSelecting= true;
+    } else {
+        runner.userData.isSelecting= false;
+    }
+}
 
 export const vrConsole={
     init: function (){ 
+        vr.vrbRender= vr.render;
+        vr.render= ()=>{
+            pipeline();
+            vr.vrbRender();
+        }
       // consoleVR
         let cFrame, cScreen, cPlane;
         cFrame = new THREE.Mesh(
@@ -49,5 +69,16 @@ export const vrConsole={
         cPlane= createText( d20(), .1 );
         cPlane.position.z+= .02;
         cScreen.add( cPlane );
+
+        runner = new THREE.Mesh( 
+            new THREE.CapsuleGeometry( .1, .2, 3, 5).rotateZ(3.14/2).translate( -.1, 1.5, -.2),
+            	new THREE.MeshBasicMaterial( {color: 0x330099, wireframe: true} ) 
+          );
+			  	vr.scene.add( runner );
+
+			  	testLight = new THREE.Mesh( 
+            new THREE.BoxGeometry( .1, .1, .1 ).translate( .3, 1.55, -.3 ),
+            new THREE.MeshBasicMaterial( {color: 0x777777} ) );
+		  		vr.scene.add( testLight );
     }
   };
