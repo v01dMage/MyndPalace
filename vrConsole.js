54844ea6ae20a6b82cebd38a36102977f85f645e
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-04-21 09:28:36 -0400

    Update vrConsole.js

--- vrConsole.js
+++ vrConsole.js
@@ -1,6 +1,30 @@
 //separate console logic
+import * as THREE from'three';
 
 // init( c ) where c is a controller 
   export const vrConsole={
-    init: function ( c ){}
+    init: function ( c ){ // c= controllerGrip1
+      // consoleVR
+        let cFrame, cScreen, cPlane;
+        cFrame = new THREE.Mesh(
+            new THREE.BoxGeometry( .1, .1, .01 ),
+            new THREE.MeshPhongMaterial( {color: 0x229933} ) );
+        c.add( cFrame );
+
+        cScreen= new THREE.Mesh(
+            new THREE.BoxGeometry( .08, .06, .001).translate( 0, .01, .01),
+            new THREE.MeshBasicMaterial( {
+                color: 0x110011,
+                wireframe: false
+            })
+          );
+        cFrame.add( cScreen );
+        cFrame.rotateX( -1.67 );
+        cFrame.position.y+= .04;
+        cFrame.position.z+= .11;
+
+        cPlane= createText( d20(), .1 );
+        cPlane.position.z+= .02;
+        cScreen.add( cPlane );
+    }
   };
