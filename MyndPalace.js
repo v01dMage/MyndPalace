99659a56af16020d5ee82fa1b6039adc6b4b219a
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-04-21 15:04:59 -0400

    Update MyndPalace.js

--- MyndPalace.js
+++ MyndPalace.js
@@ -1,10 +1,10 @@
 //Main entry to vr lab
 
-//import * as vr from 'vr/VRBoilerplate.js'; 
+import * as vr from 'vr/VRBoilerplate.js'; 
 //import 'vr/vrConsole.js';
 
 //have fun
 
 let t= document.createElement('div');
-t.innerHTML= typeof vr;
+t.innerHTML= typeof vr+' salt';
 document.body.appendChild(t);
