506a75a9d02cbd3f217cc69503044d9431e7f89b
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-04-21 16:50:37 -0400

    uncomment most. issues fixed.

--- VRBoilerplate.js
+++ VRBoilerplate.js
@@ -27,163 +27,153 @@ let pipeline= [render];
  const tempMatrix = new THREE.Matrix4();
 
  const self= {};
-//
-function init(){}
-//
-//
-//
+
+
 /*
 init();
 animate();
 */
- /*
- function init() {
-        self.scene= scene = new THREE.Scene();
-        scene.background = new THREE.Color( 0x104030 );
 
-        camera = new THREE.PerspectiveCamera( 
-            50, window.innerWidth / window.innerHeight, 0.1, 1000 );
-        camera.position.set( 0, 1, 3 );
+function init() {
+  self.scene= scene = new THREE.Scene();
+  scene.background = new THREE.Color( 0x104030 );
 
-        room = new THREE.LineSegments(
-            new BoxLineGeometry( 5, 5, 5, 5, 4, 3 ).translate( 0, 2.5, 0 ),
-            new THREE.LineBasicMaterial( { color: 0xbc44bc } )
-          );
-        scene.add( room );
+  camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  camera.position.set( 0, 1, 3 );
 
-        testText= createText( d20(), .3 );
-        testText.rotateX(- .7 );
-        testText.position.z= -.75;
-        testText.position.y= 1;
-        scene.add( testText ); 
+  room = new THREE.LineSegments(
+           new BoxLineGeometry( 5, 5, 5, 5, 4, 3 ).translate( 0, 2.5, 0 ),
+           new THREE.LineBasicMaterial( { color: 0xbc44bc } )
+         );
+  scene.add( room );
+
+  testText= createText( d20(), .3 );
+  testText.rotateX(- .7 );
+  testText.position.z= -.75;
+  testText.position.y= 1;
+  scene.add( testText ); 
 
                                
-        scene.add( new THREE.HemisphereLight( 0xa5a5a5, 0x898989, 3 ) );
+  scene.add( new THREE.HemisphereLight( 0xa5a5a5, 0x898989, 3 ) );
 
-        const light = new THREE.DirectionalLight( 0xffffff, 3 );
-        light.position.set( 1, 1, 1 ).normalize();
-        scene.add( light );
+  const light = new THREE.DirectionalLight( 0xffffff, 3 );
+  light.position.set( 1, 1, 1 ).normalize();
+  scene.add( light );
 
-        marker = new THREE.Mesh(
+  marker = new THREE.Mesh(
             new THREE.CircleGeometry( 0.25, 32 ).rotateX( - Math.PI / 2 ),
             new THREE.MeshBasicMaterial( { color: 0xbcbcbc } )
           );
-        scene.add( marker );
+  scene.add( marker );
 
-        floor = new THREE.Mesh(
+  floor = new THREE.Mesh(
             new THREE.PlaneGeometry( 4.8, 4.8, 2, 2 ).rotateX( - Math.PI / 2 ),
             new THREE.MeshBasicMaterial( { color: 0x2222bc, transparent: true, opacity: 0.25 } )
           );
-        scene.add( floor );
+  scene.add( floor );
 
-        raycaster = new THREE.Raycaster();
-	 self.raycaster= raycaster;
+  raycaster = new THREE.Raycaster();
+  self.raycaster= raycaster;
 
-        renderer = new THREE.WebGLRenderer( { antialias: true } );
-        renderer.setPixelRatio( window.devicePixelRatio );
-        renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
 
-        renderer.xr.addEventListener( 'sessionstart', 
+  renderer.xr.addEventListener( 'sessionstart', 
           () => baseReferenceSpace = renderer.xr.getReferenceSpace() 
           );
-        renderer.xr.enabled = true;
+  renderer.xr.enabled = true;
 
-        document.body.appendChild( renderer.domElement );
-        document.body.appendChild( VRButton.createButton( renderer ) );
-
-
-        // controllers
-
-        function onSelectStart() {
-          this.userData.isSelecting = true;
-        };
-
-        function onSelectEnd() {
-          this.userData.isSelecting = false;
-          };
-
-          if ( INTERSECTION ) {
-            const offsetPosition = { 
-                x: - INTERSECTION.x, y: - INTERSECTION.y, z: - INTERSECTION.z, w: 1 
-              };
-            const offsetRotation = new THREE.Quaternion();
-            const transform = new XRRigidTransform( offsetPosition, offsetRotation );
-            const teleportSpaceOffset = baseReferenceSpace.getOffsetReferenceSpace( transform );
-
-            renderer.xr.setReferenceSpace( teleportSpaceOffset );
-          }
-        }
-
-        controller1 = renderer.xr.getController( 0 );
-        self.controller1= controller1;
-        controller1.addEventListener( 'selectstart', onSelectStart );
-        controller1.addEventListener( 'selectend', onSelectEnd );
-        controller1.addEventListener( 'connected', function ( event ) {
-            this.add( buildController( event.data ) );
-          } );
-        controller1.addEventListener( 'disconnected', function () {
-            this.remove( this.children[ 0 ] );
-          } );
-        scene.add( controller1 );
-
-        controller2 = renderer.xr.getController( 1 );
-        self.controller2= controller2;
-        controller2.addEventListener( 'selectstart', onSelectStart );
-        controller2.addEventListener( 'selectend', onSelectEnd );
-        controller2.addEventListener( 'connected', function ( event ) {
-            this.add( buildController( event.data ) );
-          } );
-        controller2.addEventListener( 'disconnected', function () {
-            this.remove( this.children[ 0 ] );
-          } );
-        scene.add( controller2 );
+  document.body.appendChild( renderer.domElement );
+  document.body.appendChild( VRButton.createButton( renderer ) );
 
 
-        // The XRControllerModelFactory will automatically fetch controller models
-        // that match what the user is holding as closely as possible. The models
-        // should be attached to the object returned from getControllerGrip in
-        // order to match the orientation of the held device.
+  // controllers
 
-        const controllerModelFactory = new XRControllerModelFactory();
+  function onSelectStart() {
+    this.userData.isSelecting = true;
+  }
 
-        controllerGrip1 = renderer.xr.getControllerGrip( 0 );
-self.controllerGrip1= controllerGrip1;
-        controllerGrip1.add( controllerModelFactory.createControllerModel( controllerGrip1 ) );
-        scene.add( controllerGrip1 );
+  function onSelectEnd() {
+    this.userData.isSelecting = false;
 
-        controllerGrip2 = renderer.xr.getControllerGrip( 1 );
-self.controllerGrip2= controllerGrip2;
-        controllerGrip2.add( controllerModelFactory.createControllerModel( controllerGrip2 ) );
-        scene.add( controllerGrip2 );
+    if ( INTERSECTION ) {
+       const offsetPosition = { 
+          x: - INTERSECTION.x, y: - INTERSECTION.y, z: - INTERSECTION.z, w: 1 
+       };
+       const offsetRotation = new THREE.Quaternion();
+       const transform = new XRRigidTransform( offsetPosition, offsetRotation );
+       const teleportSpaceOffset = baseReferenceSpace.getOffsetReferenceSpace( transform );
 
-        //
-        window.addEventListener( 'resize', onWindowResize, false );
-      }
+       renderer.xr.setReferenceSpace( teleportSpaceOffset );
+    }
+  }
 
-     function buildController( data ) {
-        let geometry, material;
+  controller1 = renderer.xr.getController( 0 );
+  self.controller1= controller1;
+  controller1.addEventListener( 'selectstart', onSelectStart );
+  controller1.addEventListener( 'selectend', onSelectEnd );
+  controller1.addEventListener( 'connected', function ( event ) {
+        this.add( buildController( event.data ) );
+  } );
+  controller1.addEventListener( 'disconnected', function () {
+        this.remove( this.children[ 0 ] );
+  } );
+  scene.add( controller1 );
 
-        switch ( data.targetRayMode ) {
-          case 'tracked-pointer':
-            geometry = new THREE.BufferGeometry();
-            geometry.setAttribute( 'position', 
+  controller2 = renderer.xr.getController( 1 );
+  self.controller2= controller2;
+  controller2.addEventListener( 'selectstart', onSelectStart );
+  controller2.addEventListener( 'selectend', onSelectEnd );
+  controller2.addEventListener( 'connected', function ( event ) {
+        this.add( buildController( event.data ) );
+  } );
+  controller2.addEventListener( 'disconnected', function () {
+        this.remove( this.children[ 0 ] );
+  } );
+  scene.add( controller2 );
+
+
+  const controllerModelFactory = new XRControllerModelFactory();
+
+  controllerGrip1 = renderer.xr.getControllerGrip( 0 );
+  self.controllerGrip1= controllerGrip1;
+  controllerGrip1.add( controllerModelFactory.createControllerModel( controllerGrip1 ) );
+  scene.add( controllerGrip1 );
+
+  controllerGrip2 = renderer.xr.getControllerGrip( 1 );
+  self.controllerGrip2= controllerGrip2;
+  controllerGrip2.add( controllerModelFactory.createControllerModel( controllerGrip2 ) );
+  scene.add( controllerGrip2 );
+
+  //
+  window.addEventListener( 'resize', onWindowResize, false );
+}
+
+function buildController( data ) {
+  let geometry, material;
+
+  switch ( data.targetRayMode ) {
+    case 'tracked-pointer':
+       geometry = new THREE.BufferGeometry();
+       geometry.setAttribute( 'position', 
               new THREE.Float32BufferAttribute( [ 0, 0, 0, 0, 0, - 2 ], 3 ) 
-              );
-            geometry.setAttribute( 'color', 
+       );
+       geometry.setAttribute( 'color', 
               new THREE.Float32BufferAttribute( [ 0.5, 0.2, 0.5, 0, 0, 0 ], 3 ) 
-              );
-            material = new THREE.LineBasicMaterial( { 
-                vertexColors: true, blending: THREE.AdditiveBlending 
-              } );
-            return new THREE.Line( geometry, material );
+       );
+       material = new THREE.LineBasicMaterial( { 
+              vertexColors: true, blending: THREE.AdditiveBlending 
+       } );
+      return new THREE.Line( geometry, material );
 
-          case 'gaze':
-            geometry = new THREE.RingGeometry( 0.02, 0.04, 32 ).translate( 0, 0, - 1 );
-            material = new THREE.MeshBasicMaterial( { opacity: 0.5, transparent: true } );
-            return new THREE.Mesh( geometry, material );
-        }
-      }
-*/
+    case 'gaze':
+       geometry = new THREE.RingGeometry( 0.02, 0.04, 32 ).translate( 0, 0, - 1 );
+       material = new THREE.MeshBasicMaterial( { opacity: 0.5, transparent: true } );
+      return new THREE.Mesh( geometry, material );
+  }
+}
+
 
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -192,6 +182,7 @@ function onWindowResize() {
   renderer.setSize( window.innerWidth, window.innerHeight );
 }
   
+
 function addToPipeline(f){
   pipeline.unshift(f);
 }
@@ -238,6 +229,6 @@ function render() {
   renderer.render( scene, camera );
 }
 
-let saltwater 
+let pinksalt;
 
-export { init, animate, updatePipeline, addToPipeline, render, self, saltwater };
+export { init, animate, updatePipeline, addToPipeline, render, self, pinksalt };
