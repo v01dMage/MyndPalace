b92a7c2cb1ffe89027ce9aff1b874e9d478f1b22
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-04-21 11:10:17 -0400

    hm.

--- MyndPalace.js
+++ MyndPalace.js
@@ -1,10 +1,8 @@
 //Main entry to vr lab
 
-import { vr, init, animate } from 'vr/VRBoilerplate.js'; 
+import { vr } from 'vr/VRBoilerplate.js'; 
 import { vrConsole } from 'vr/vrConsole.js';
 
 export function enter(){
-  init();
   vrConsole.init();
-  animate();
 }
