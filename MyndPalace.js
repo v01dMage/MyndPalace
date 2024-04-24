fa712ed48e3bd9f67aed8dc16158bd20acfa7abf
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-04-24 15:22:07 -0400

    preload test text

--- MyndPalace.js
+++ MyndPalace.js
@@ -5,6 +5,19 @@ import 'vr/vrConsole.js';
 
 //have fun
 
-let t= document.createElement('div');
-t.innerHTML= Object.keys(vr).join('<br>');
-document.body.appendChild(t);
+let ta= document.getElementById('consoleInput');
+ta.value=`import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+
+const loader = new GLTFLoader();
+
+loader.load( './assets/banana_3d_scanned.glb', function ( gltf ) {
+
+	vr.scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+  let errdiv= document.createElement('div');
+  errdiv.innerHTML = error;
+  document.body.appendChild( errdiv );
+
+} );`;
