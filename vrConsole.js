c6820c0fa1b905d91cfd89b19c681bf09142fe7c
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-04-24 15:40:32 -0400

    async

--- vrConsole.js
+++ vrConsole.js
@@ -35,7 +35,7 @@ function onSelectEnd(){
        let text= document.getElementById( 'consoleInput' );
        let out;
        try{
-          out= (Function(text.value))();
+          out= (async Function(text.value))();
        } catch(err){
           out= err;
        }
