b9b5bac41fefa849e6948bc15f970f261243b853
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-04-25 15:08:46 -0400

    load button(s), change default test

--- MyndPalace.js
+++ MyndPalace.js
@@ -2,22 +2,9 @@
 
 import * as vr from 'vr/VRBoilerplate.js'; 
 import 'vr/vrConsole.js';
+import 'vr/assets/miscButtons';
 
 //have fun
 
 let ta= document.getElementById('consoleInput');
-ta.value=`const { GLTFLoader }= await import( 'three/addons/loaders/GLTFLoader.js' )
-
-const loader = new GLTFLoader();
-
-loader.load( './banana_3d_scanned.glb', function ( gltf ) {
-
-	vr.self.scene.add( gltf.scene );
-
-}, undefined, function ( error ) {
-
-  let errdiv= document.createElement('div');
-  errdiv.innerHTML = error;
-  document.body.appendChild( errdiv );
-
-} );`;
+ta.value=`return 'Hi';`;
