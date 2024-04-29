ff17f5357335ae192c2368fd1bb330545a1ea9a1
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-04-29 17:53:58 -0400

    tweak.

--- vrConsole.js
+++ vrConsole.js
@@ -85,7 +85,7 @@ async function init(){
   testText.rotateX(- .7 );
   testText.rotateY( 1.4);
   testText.position.z= .24;
-  testText.position.y= -.07;
+  testText.position.y=  .07;
   testText.position.x= -.13;
   vr.self.controllerGrip1.add( testText );
 
