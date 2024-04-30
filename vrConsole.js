c28cc69a88e369fa1bbe679aeba226feeeec18ed
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-04-30 15:45:31 -0400

    Update vrConsole.js

--- vrConsole.js
+++ vrConsole.js
@@ -78,7 +78,7 @@ function makePoint( parent, x, y, z ){
 async function init(){ 
 
    let positions= [
-     {x: 0, y: 0, z: .1},
+     {x: 0, y: 0, z: -.1},
      {x: .1, y: 0, z: 0},
      {x: -.1, y: 0, z: 0},
      {x: 0, y: .1, z: 0},
