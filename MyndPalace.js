14c60a965ab1e5268f0539b823a5fa7368a24e6e
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-04 17:26:16 -0400

    uncomment teleport loader

--- MyndPalace.js
+++ MyndPalace.js
@@ -1,7 +1,7 @@
 //Main entry to vr lab
 
 import * as vr from 'vr/VRBoilerplate.js'; 
-//import Floor from 'vr/vrTeleport.js';
+import 'vr/vrTeleport.js';
 import 'vr/vrConsole.js';
 import 'vr/assets/miscButtons.js';
 
