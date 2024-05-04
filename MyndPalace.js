415c83d20aef88f14598d57bcdefdfd01cec0cdf
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-04 18:21:24 -0400

    .

--- MyndPalace.js
+++ MyndPalace.js
@@ -6,6 +6,7 @@ import * as vr from 'vr/VRBoilerplate.js';
 //import 'vr/assets/miscButtons.js';
 
 //have fun
-
+/*
 let ta= document.getElementById('consoleInput');
 ta.value= localStorage.current;
+*/
