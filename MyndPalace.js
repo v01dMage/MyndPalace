524be5b3ae407fb21de55d8c7b17bbc600d3e898
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-04 19:38:58 -0400

    bring it all back

--- MyndPalace.js
+++ MyndPalace.js
@@ -1,12 +1,11 @@
 //Main entry to vr lab
 
 import * as vr from 'vr/VRBoilerplate.js'; 
-//import 'vr/vrTeleport.js';
-//import 'vr/vrConsole.js';
-//import 'vr/assets/miscButtons.js';
+import 'vr/vrTeleport.js';
+import 'vr/vrConsole.js';
+import 'vr/assets/miscButtons.js';
 
 //have fun
-/*
+
 let ta= document.getElementById('consoleInput');
 ta.value= localStorage.current;
-*/
