2ace20cc35e3c109cb6c4b5dfabb3490fe374987
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-05 11:10:05 -0400

    Create miscButtons.js

---
+++ cantrips/miscButtons.js
@@ -0,0 +1,25 @@
+// misc button examples and fun
+
+let button;
+button= document.createElement('button');
+button.innerHTML= 'glbBanana';
+button.addEventListener('click', function(){
+
+let ta= document.getElementById('consoleInput');
+ta.value=`const { GLTFLoader }= await import( 'three/addons/loaders/GLTFLoader.js' )
+
+const loader = new GLTFLoader();
+
+loader.load( './banana_3d_scanned.glb', function ( gltf ) {
+
+        vr.self.scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+  let errdiv= document.createElement('div');
+  errdiv.innerHTML = error;
+  document.body.appendChild( errdiv );
+
+} );`;
+});
+document.body.appendChild(button);
