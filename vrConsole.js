d06e778d71cb5d36bfa8f247692ce91a44d203c3
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-04 20:15:08 -0400

    remove pyramid from console

--- vrConsole.js
+++ vrConsole.js
@@ -69,26 +69,7 @@ async function arun(text){
     return out;
 }
 
-function makePoint( parent, x, y, z, color ){
-    let mesh= new THREE.Mesh(
-      new THREE.SphereGeometry( 0.01 ).translate( x, y, z ),
-      new THREE.MeshBasicMaterial( {color} )
-    );
-    parent.add( mesh );
-}
-
 async function init(){ 
-
-   let points= [
-     {x: 0, y: 0, z: -.1, color: 0xaaaaff},
-     {x: .1, y: 0, z: 0, color: 0xaaaaaa},
-     {x: -.1, y: 0, z: 0, color: 0x777777},
-     {x: 0, y: .1, z: 0, color: 0xaaaa00},
-     {x: 0, y: -.1, z: 0, color: 0x777700}
-   ];
-   points.forEach( ({x,y,z,color})=>{
-     makePoint( vr.self.controllerGrip2, x, y, z, color );
-   });
 
    let ta= document.createElement('textarea');
    ta.setAttribute( 'rows', '24' );
