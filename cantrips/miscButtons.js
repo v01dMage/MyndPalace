f9c7f328d4c98547b7857538807c2f8e44e84614
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-05 22:34:23 -0400

    add store to current

--- cantrips/miscButtons.js
+++ cantrips/miscButtons.js
@@ -16,6 +16,9 @@ loader.load( './assets/banana_3d_scanned.glb', function ( gltf ) {
   document.body.appendChild( errdiv );
 
 } );`,
+  'current' : `let ta= document.getElementById('consoleInput');
+localStorage.current= ta.value;
+return ':)';`,
 
   };
 makeButtons(buttons);
