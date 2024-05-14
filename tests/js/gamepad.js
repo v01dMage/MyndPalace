e966ba1a4b704af4cb63c88ad2aa8ec462e94ed4
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-14 16:53:15 -0400

    scoping. going deeper.

--- tests/js/gamepad.js
+++ tests/js/gamepad.js
@@ -5,11 +5,17 @@ let xr= vr.self.renderer.xr;
 let cout= vr.self.console.cout;
 let delay= 3000;
 let polling;
+let count= 0;
 
 polling= ()=>{
   if( xr.isPresenting ){
+    count++;
+    cout( count );
     let session= xr.getSession();
     cout( session.inputSources );
+    for( let src of session.inputSources ){
+      cout( src ); cout( src?.gamepad )
+    }
     cout( '***' )
   }
 
