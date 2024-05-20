3aceadb46dc4b1d59a93da885b0630a0fb5b9fac
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-20 16:51:50 -0400

    copy from testing zone

--- avatar/gamepad.js
+++ avatar/gamepad.js
@@ -1,3 +1,50 @@
 //Gamepad
 //
 
+import * as vr from 'vr/vr.js';
+
+const gamepad= { 
+  leftXaxis: 0,
+  leftYaxis: 0,
+  leftAxisButton: false,
+  rightXaxis: 0,
+  rightYaxis: 0,
+  rightAxisButton: false,
+  leftTrigger: 0,
+  rightTrigger: 0,
+  leftGrip: 0,
+  rightGrip: 0,
+  A: false,
+  B: false,
+  X: false,
+  Y: false,
+  haptics: { left: undefined, right: undefined},
+  pulse: pulse,
+ };
+vr.self.gamepad= gamepad;
+
+let gamepadRecon= (o)=>{
+  if( xr.isPresenting ){
+  let ins= xr.getSession().inputSources;
+  ins.forEach( src=>{
+    if(src.gamepad){
+      let h= src.handedness;
+      let l= h == 'left';
+      let btns= src.gamepad.buttons;
+      gamepad[h+'Xaxis']= src.gamepad.axes[2];
+      gamepad[h+'Yaxis']= src.gamepad.axes[3];
+      gamepad[h+'Trigger']= btns[0].value;
+      gamepad[h+'Grip']= btns[1].value;
+      gamepad[h+'AxisButton']= btns[3].pressed;
+      gamepad[l?'X':'A']= btns[4].pressed;
+      gamepad[l?'Y':'B']= btns[5].pressed;
+      gamepad.haptics[h]= src.gamepad.hapticActuators[0];
+    }
+  } );}
+}
+
+vr.addToRecon( gamepadRecon );
+
+function pulse(hand, strength, ms){
+  let pulsed= gamepad.haptics[hand].pulse( strength, ms );
+}
