6c83ac85517f6a4fed94edfd17e2b75a11184fc0
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-22 17:40:46 -0400

    swapRealm attempt

--- tests/js/scenes.js
+++ tests/js/scenes.js
@@ -4,3 +4,33 @@
 import * as vr from 'vr/vr.js';
 import * as THREE from 'three';
 
+//add controllers and grips,
+// background, lights, etc 
+
+let voyd= new THREE.Scene();
+voyd.background = new THREE.Color( 0xeeffee );
+
+voyd.add( vr.self.controller1 );
+voyd.add( vr.self.controller2 );
+voyd.add( vr.self.controllerGrip1 );
+voyd.add( vr.self.controllerGrip2 );
+
+voyd.add( new THREE.HemisphereLight( 0xcccccc, 0x779977, 3 ) );
+
+vr.self.scenes.push( voyd );
+
+let lastSwapped= Date.now();
+function swapRealm(o){
+  if(Date.now() - lastSwapped> 2000){
+    let gp= vr.self.gamepad;
+    if(gp.leftGrip > 0.3 && gp.Y){
+      vr.self.scenes.push(
+        vr.self.scenes.unshift();
+      );
+      lastSwapped= Date.now();
+    }
+  }
+}
+
+vr.addToUpdate( swapRealm );
+
