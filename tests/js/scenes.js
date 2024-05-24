624de1a1d7edf1d903a6473718af9f0718c22f45
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-24 15:31:30 -0400

    move hands when hopping

--- tests/js/scenes.js
+++ tests/js/scenes.js
@@ -9,20 +9,21 @@ import * as THREE from 'three';
 // background, lights, etc 
 
 let voyd= new THREE.Scene();
-voyd.background = new THREE.Color( 0xeeffee );
+voyd.background = new THREE.Color( 0x77bb88 );
 
-voyd.add( vr.self.controller1 );
-voyd.add( vr.self.controller2 );
-voyd.add( vr.self.controllerGrip1 );
-voyd.add( vr.self.controllerGrip2 );
+let my= vr.self;
+my.hands= [ 
+  my.controller1, my.controller2,
+  my.controllerGrip1, my.controllerGrip2
+];
 
 voyd.add( new THREE.HemisphereLight( 0xcccccc, 0x779977, 3 ) );
 
-vr.self.scenes.push( voyd );
+my.scenes.push( voyd );
 
 let lastSwapped= Date.now();
-let gp= vr.self.gamepad;
-let scenes= vr.self.scenes;
+let gp= my.gamepad;
+let scenes= my.scenes;
 
 function swapRealm(o){
   if(Date.now() - lastSwapped> 2000){
@@ -31,6 +32,11 @@ function swapRealm(o){
       let temp= scenes.shift();
       scenes.push( temp );
       lastSwapped= Date.now();
+
+      let realm= scenes[0];
+      my.hands.forEach( hand=>{
+        realm.add( hand );
+      } );
     }
   }
 }
