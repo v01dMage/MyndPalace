2cb7b9dddb454021170ff00a07dd0b4b9505859a
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-25 14:35:51 -0400

    add skybox to voyd realm

--- MyndPalace.js
+++ MyndPalace.js
@@ -3,10 +3,11 @@
 import * as vr from 'vr/vr.js'; 
 import 'vr/teleport.js';
 import 'vr/console.js';
-//import 'vr/skybox.js';
 import 'my/cantrips/miscButtons.js';
 import 'vr/gamepad.js';
-import 'vr/scenes.js';
+import 'vr/scenes.js'; //needs gamepad
+import 'vr/skybox.js'; //voyd scene
+
 
 //have fun
 
