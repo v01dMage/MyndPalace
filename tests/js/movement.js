dfbf8f9a7c4fd4671a59bb46344fb2a832c9a887
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-27 19:38:46 -0400

    try border

--- tests/js/movement.js
+++ tests/js/movement.js
@@ -12,15 +12,20 @@ import * as THREE from 'three';
 // other with clover5 jpg
 
 let radius= .5;
+let height= .1;
 let borderDifference= .05;
 //loader promise for texture
 // disc in then?
 
-/*
+let r= radius+ radius* borderDifference;
+let h= height- height*borderDifference;
 let border= new THREE.Mesh(
-  new THREE.?CylinderGeometry(?),
-  new THREE.?meshBasicMaterial(?)
+  new THREE.CylinderGeometry(r,r,h, 16, 1, true ),
+  new THREE.MeshBasicMaterial( {color: 0x000000} )
 );
+ve.self.scenes[0].add( border );
+
+/*
 let disc= new THREE.Mesh(
   new THREE.?CylinderGeometry(?),
   new THREE.? texture
