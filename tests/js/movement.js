539821122aab08fa7961ffe4817b48ffb518cbbc
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-27 19:23:07 -0400

    movement blather to research

--- tests/js/movement.js
+++ tests/js/movement.js
@@ -2,8 +2,29 @@
 // maybe perspective shift
 
 import * as vr from 'vr/vr.js';
+import * as THREE from 'three';
 
 //Attach camera to a floating disc,
 //  and use the gamepad for movement 
+// WARN: incompatible with teleport?
+
+// 2 cylinder discs, 1 thinner and wider
+// other with clover5 jpg
+
+let radius= .5;
+let borderDifference= .05;
+//loader promise for texture
+// disc in then?
+
+/*
+let border= new THREE.Mesh(
+  new THREE.?CylinderGeometry(?),
+  new THREE.?meshBasicMaterial(?)
+);
+let disc= new THREE.Mesh(
+  new THREE.?CylinderGeometry(?),
+  new THREE.? texture
+);
+*/
 
 vr.self.console.cout( 'movement loaded' );
