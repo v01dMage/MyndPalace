996fa37f3895bc01cf79e051ed559ca9e554384b
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-27 09:02:39 -0400

    move plans

--- tests/js/movement.js
+++ tests/js/movement.js
@@ -1,6 +1,9 @@
-//Walking and rotate cotrols
+//Walking and rotate controls
 // maybe perspective shift
 
 import * as vr from 'vr/vr.js';
+
+//Attach camera to a floating disc,
+//  and use the gamepad for movement 
 
 vr.self.console.cout( 'movement loaded' );
