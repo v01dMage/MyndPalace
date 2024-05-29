be452b5f00d4fce0fbddc6ec6c3b01ae15646c94
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-29 18:29:12 -0400

    begin again

--- avatar/cns.js
+++ avatar/cns.js
@@ -3,3 +3,17 @@
 //  import and initialize base xr system
 
 import * as THREE from 'three';
+import { VRButton } from 'three/addons/webxr/VRButton.js';
+import { XRControllerModelFactory } from 'three/addons/webxr/XRControllerModelFactory.js';
+import { MyMeta3Gamepad } from 'xr/gamepad.js';
+
+let camera, scene, renderer;
+let raycasterLeft, raycasterRight;
+let recon= [basicRecon];
+let update= [basicUpdate];
+
+export const tempMatrix = new THREE.Matrix4();
+
+export const self= {};
+let controller1, controller2;
+let controllerGrip1, controllerGrip2;
