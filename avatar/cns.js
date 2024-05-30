43caf267ffdf7b73ce8315d6b8cc93fa3d518701
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-29 22:50:51 -0400

    add pointer fn

--- avatar/cns.js
+++ avatar/cns.js
@@ -40,3 +40,27 @@ function basicRecon(o) {
   }
   
 }
+
+function buildPointer( data ) {
+  let geometry, material;
+
+  switch ( data.targetRayMode ) {
+    case 'tracked-pointer':
+       geometry = new THREE.BufferGeometry();
+       geometry.setAttribute( 'position', 
+              new THREE.Float32BufferAttribute( [ 0, 0, 0, 0, 0, - 2 ], 3 ) 
+       );
+       geometry.setAttribute( 'color', 
+              new THREE.Float32BufferAttribute( [ 0.5, 0.2, 0.5, 0, 0, 0 ], 3 ) 
+       );
+       material = new THREE.LineBasicMaterial( { 
+              vertexColors: true, blending: THREE.AdditiveBlending 
+       } );
+      return new THREE.Line( geometry, material );
+
+    case 'gaze':
+       geometry = new THREE.RingGeometry( 0.02, 0.04, 32 ).translate( 0, 0, - 1 );
+       material = new THREE.MeshBasicMaterial( { opacity: 0.5, transparent: true } );
+      return new THREE.Mesh( geometry, material );
+  }
+}
