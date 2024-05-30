7debdc71ad231c5edbf3685fd933d578759876bf
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-29 22:42:45 -0400

    more

--- avatar/cns.js
+++ avatar/cns.js
@@ -19,3 +19,22 @@ self.hands= [];
 let controller1, controller2;
 let controllerGrip1, controllerGrip2;
 
+
+
+
+
+function basicRecon(o) {
+  if ( controller1.userData.isSelecting === true ) {
+    tempMatrix.identity().extractRotation( controller1.matrixWorld );
+
+    raycasterLeft.ray.origin.setFromMatrixPosition( controller1.matrixWorld );
+    raycasterLeft.ray.direction.set( 0, 0, - 1 ).applyMatrix4( tempMatrix );
+  }
+  if ( controller2.userData.isSelecting === true ) {
+    tempMatrix.identity().extractRotation( controller2.matrixWorld );
+
+    raycasterRight.ray.origin.setFromMatrixPosition( controller2.matrixWorld );
+    raycasterRight.ray.direction.set( 0, 0, - 1 ).applyMatrix4( tempMatrix );
+  }
+  o.xr= xr;
+}
