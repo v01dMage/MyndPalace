248db115b3de98d97e50b03366036f9aa66f0632
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-29 22:37:59 -0400

    continuing

--- avatar/cns.js
+++ avatar/cns.js
@@ -5,15 +5,17 @@
 import * as THREE from 'three';
 import { VRButton } from 'three/addons/webxr/VRButton.js';
 import { XRControllerModelFactory } from 'three/addons/webxr/XRControllerModelFactory.js';
-import { MyMeta3Gamepad } from 'xr/gamepad.js';
+import { gamepad } from 'xr/gamepad.js';//MetaQ3
 
-let camera, scene, renderer;
+let camera, scene, scenes, renderer, xr;
 let raycasterLeft, raycasterRight;
-let recon= [basicRecon];
-let update= [basicUpdate];
+let recon= [ basicRecon, gamepad.recon ];
+let update= [ basicUpdate ];
 
 export const tempMatrix = new THREE.Matrix4();
 
 export const self= {};
+self.hands= [];
 let controller1, controller2;
 let controllerGrip1, controllerGrip2;
+
