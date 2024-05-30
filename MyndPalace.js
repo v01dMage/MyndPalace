a20bbe39086c44c20cc987e11906123d6e5ebb42
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-29 23:32:58 -0400

    doot dee doo

--- MyndPalace.js
+++ MyndPalace.js
@@ -1,12 +1,12 @@
 //Main entry to vr lab
 
-import * as vr from 'vr/vr.js'; 
-//import 'vr/teleport.js';
-import 'vr/console.js';
+import * as avatar from 'xr/cns.js'; 
+
+import 'xr/console.js';
 import 'my/cantrips/miscButtons.js';
-import 'vr/gamepad.js';
-import 'vr/scenes.js'; //needs gamepad
-import 'vr/skybox.js'; //voyd scene
+
+import 'xr/scenes.js'; //needs gamepad
+import 'xr/skybox.js'; //voyd scene
 
 if(localStorage.currentlyTesting)
   import(localStorage.currentlyTesting);
