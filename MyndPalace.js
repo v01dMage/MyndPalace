e4d44086570fc279313a997e0cf83b3d91cf6966
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-29 23:57:38 -0400

    Update MyndPalace.js

--- MyndPalace.js
+++ MyndPalace.js
@@ -5,8 +5,8 @@ import * as avatar from 'xr/cns.js';
 import 'xr/console.js';
 import 'my/cantrips/miscButtons.js';
 
-import 'xr/scenes.js'; //needs gamepad
-import 'xr/skybox.js'; //voyd scene
+import 'xr/scenes.js'; 
+import 'xr/skybox.js'; 
 
 if(localStorage.currentlyTesting)
   import(localStorage.currentlyTesting);
