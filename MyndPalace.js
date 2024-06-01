f06f6f7f7217e99c16e899d5de224d390efb75e7
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-05-31 23:55:45 -0400

    Update MyndPalace.js

--- MyndPalace.js
+++ MyndPalace.js
@@ -15,7 +15,7 @@ if(localStorage.currentlyTesting)
 let t= document.createElement('div');
 t.innerHTML= 'here';
 try{
-  t.innerHTML= avatar.gamepad.Y;
+  t.innerHTML= avatar.self.error;
 }catch(err){ t.innerHTML= err;}
 document.body.appendChild(t);
 
