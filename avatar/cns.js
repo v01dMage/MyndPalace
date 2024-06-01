e7f1a7279559d7c355ab10a8ec3556fcb0142a6b
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-01 01:03:13 -0400

    idfk

--- avatar/cns.js
+++ avatar/cns.js
@@ -136,10 +136,10 @@ function init(){
 
 
   let pointOfReference = new THREE.Mesh(
-    new THREE.CubeGeometry(),
+    new THREE.CubeGeometry(3,3,3),
     new THREE.MeshBasicMaterial( {color: 0xff4477 } )
   );
-  pointOfReference.position.z= -30;
+  pointOfReference.translate( 0, 1, -30 );
   scene.add( pointOfReference );
 }
 
