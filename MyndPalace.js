b5f2229e7f4eac2a3815f95271cc411a99cdfa9c
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-03 22:35:29 -0400

    unlock console

--- MyndPalace.js
+++ MyndPalace.js
@@ -1,10 +1,10 @@
 //Main entry to vr lab
 
 import * as avatar from 'xr/cns.js'; 
-/*
+
 import 'xr/console.js';
 import 'my/cantrips/miscButtons.js';
-
+/*
 import 'xr/scenes.js'; 
 import 'xr/skybox.js'; 
 
