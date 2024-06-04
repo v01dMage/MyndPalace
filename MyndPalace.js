f7049a9b660d93b53b66ad76a8be10b8360d6c5a
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-03 23:26:05 -0400

    Unlock scenes and skybox

--- MyndPalace.js
+++ MyndPalace.js
@@ -4,10 +4,10 @@ import * as avatar from 'xr/cns.js';
 
 import 'xr/console.js';
 import 'my/cantrips/miscButtons.js';
-/*
+
 import 'xr/scenes.js'; 
 import 'xr/skybox.js'; 
-
+/*
 if(localStorage.currentlyTesting)
   import(localStorage.currentlyTesting);
 */
