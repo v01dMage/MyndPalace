3d0bdad1706a69609f42ba9033920ab4d4c6415c
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-03 21:25:55 -0400

    change border, hopefully stop clipping

--- avatar/cns.js
+++ avatar/cns.js
@@ -107,7 +107,7 @@ function init(){
 
   let radius= .5;
   let height= .1;
-  let borderDifference= .05;
+  let borderDifference= .2;
 
   let r= radius+ radius* borderDifference;
   let h= height- height*borderDifference;
