4887d677bccc368cad68f853c51557a043309397
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-09 22:43:58 -0400

    flubadubbub

--- MyndPalace.js
+++ MyndPalace.js
@@ -1,7 +1,6 @@
 //Main entry to vr lab
 
 import * as avatar from 'xr/cns.js'; 
-
 import 'xr/console.js';
 import { cantrips } from 'my/cantrips/miscButtons.js';
 import 'xr/scenes.js'; 
@@ -12,11 +11,10 @@ if(localStorage.autoRun){
   avatar.self.console.arun(localStorage.autoRun).then(
     res=>{ avatar.self.console.cout(res); }
   );
+} else {
+  avatar.self.cantrips.cast('hello');
 }
-init();
 
-function init(){
-avatar.self.cantrips.cast('hello');}
 //have fun
 
 
