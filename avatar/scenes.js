c04c9d8a7a34b857c359da034e0bf2843236d455
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-13 23:14:50 -0400

    unsweep idiot

--- avatar/scenes.js
+++ avatar/scenes.js
@@ -14,16 +14,16 @@ voyd.add( new THREE.HemisphereLight( 0xcccccc, 0x779977, 3 ) );
 
 my.scenes.push( voyd );
 
+let lastSwapped= Date.now();
 let gp= avatar.gamepad;
 let scenes= my.scenes;
 
 function swapRealm(o){
-  if(o.deltaTime > 2000){
-    
+  if(o.now- lastSwapped > 2000){
     if(gp.leftGrip > 0.3 && gp.Y){
       let temp= scenes.shift();
       scenes.push( temp );
-
+      lastSwapped= o.now();
       scenes[0].add( avatar.self.disc )
     }
   }
