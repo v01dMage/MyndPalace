77764eff16277c712a73ac2fcb57134bf34b326c
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-14 21:19:22 -0400

    randomize more than once..

--- source/cantrip/starfield.js
+++ source/cantrip/starfield.js
@@ -8,9 +8,10 @@ export default function paintStarfield(w,h,n){
     canvas.height= `${h}`;
     let ctx= canvas.getContext("2D");
     let r= (x)=>Math.floor(Math.random()*x);
-    ctx.fillStyle= `rgb(${r(256)},${r(256)},${r(256)})`;
+    
     for(let i= 0; i< n; i++){
       let [x,y]= [r(w),r(h)];
+      ctx.fillStyle= `rgb(${r(256)},${r(256)},${r(256)})`;
       ctx.fillRect( x, y, r(13)+3, r(13)+3 );
     }
     return canvas;
