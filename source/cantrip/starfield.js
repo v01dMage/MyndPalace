8a849ad44e2df19540751bf728368a1a2b5d5326
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-14 21:50:40 -0400

    try black background? idk..

--- source/cantrip/starfield.js
+++ source/cantrip/starfield.js
@@ -7,6 +7,9 @@ export default function paintStarfield(w,h,n){
     canvas.width= w;
     canvas.height= h;
     let ctx= canvas.getContext("2d");
+    ctx.fillStyle= "#000000";
+    ctx.fillRect(0,0,w,h);
+
     let r= (x)=>Math.floor(Math.random()*x);
     
     for(let i= 0; i< n; i++){
