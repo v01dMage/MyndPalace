fe908e044007d8ce8afa0e2a26dbf069c13679b9
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-15 12:32:23 -0400

    ????????

--- source/cantrip/starfield.js
+++ source/cantrip/starfield.js
@@ -2,7 +2,7 @@
 //  creates a starfield representation 
 //  returning it in a htmlcanvas element 
 
-export function paintStarfield(w,h,n){
+export function paintStarfield(w,h,n,document){
     let canvas= document.createElement('canvas');
     canvas.width= w;
     canvas.height= h;
