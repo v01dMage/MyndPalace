e646e0ec6f293c82c84162316d3a5978f69adc26
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-14 20:39:12 -0400

    create starfield painter

---
+++ source/cantrip/starfield.js
@@ -0,0 +1,16 @@
+// export a function which
+//  creates a starfield representation 
+//  returning it in a htmlcanvas element 
+
+export default function paintStarfield(w,h,n){
+    let canvas= document.createElement('canvas');
+    canvas.width= `${w}`;
+    canvas.height= `${h}`;
+    let ctx= canvas.getContext("2D");
+    let r= (x)=>Math.floor(Math.random()*x);
+    for(let i= 0; i< n; i++){
+      let [x,y]= [r(w),r(h)];
+      ctx.fillRect( x, y, r(13)+3, r(13)+3 );
+    }
+    return canvas;
+}
