7664fc7873cc0f58eb1eb1e33945702428dae586
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-14 21:17:53 -0400

    fill style might be good

--- source/cantrip/starfield.js
+++ source/cantrip/starfield.js
@@ -8,6 +8,7 @@ export default function paintStarfield(w,h,n){
     canvas.height= `${h}`;
     let ctx= canvas.getContext("2D");
     let r= (x)=>Math.floor(Math.random()*x);
+    ctc.fillStyle= `rgb(${r(256)},${r(256)},${r(256)})`;
     for(let i= 0; i< n; i++){
       let [x,y]= [r(w),r(h)];
       ctx.fillRect( x, y, r(13)+3, r(13)+3 );
