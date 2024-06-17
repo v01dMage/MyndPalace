32d79357cdbc7bd6a151b0261b63d2a321e13e0c
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-17 14:22:23 -0400

    Update MyndPalace.js

--- MyndPalace.js
+++ MyndPalace.js
@@ -31,8 +31,8 @@ avatar.self.scenes[0].background= starfield;
 const testPaper= conjurePaper();
 testPaper.position.y= .7 ;
 function rotatePaper(i){
-  testPaper.rotateY(i.deltaTime/1000);
-  testPaper.rotateX(i.deltaTime/100000);
+  //testPaper.rotateY(i.deltaTime/1000);
+  testPaper.rotateX(i.deltaTime/10000);
 }
 avatar.addToUpdate( rotatePaper );
 //have fun
