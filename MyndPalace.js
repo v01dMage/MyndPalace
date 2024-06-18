233092636c1f8736dea0a73cea0e25a279a19bed
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-18 00:27:05 -0400

    sigh.. hmm

--- MyndPalace.js
+++ MyndPalace.js
@@ -28,7 +28,7 @@ starfield.mapping = THREE.EquirectangularReflectionMapping;
 starfield.colorSpace = THREE.SRGBColorSpace;
 avatar.self.scenes[0].background= starfield;
 
-const testPaper= conjurePaper();
+const testPaper= conjurePaper(.2,.25,.5);
 testPaper.position.y= .7 ;
 function rotatePaper(i){
   //testPaper.rotateY(i.deltaTime/1000);
