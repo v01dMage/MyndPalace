7a6c7f8e9e172ae69e6d72a235450c602afc60be
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-19 13:42:39 -0400

    Update introspection.js

--- source/cantrip/introspection.js
+++ source/cantrip/introspection.js
@@ -3,7 +3,7 @@
 
 import { conjurePaper } from 'psi/cantrip/paper.js'
 
-const src= document.body.parent.outerHTML;
+const src= document.body.documentElement.outerHTML;
 
 let page= conjurePaper( src );
 page.position.y= 1;
