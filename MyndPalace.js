29ae7df2ca52b3739da965313fc1d69484ae88e3
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-20 12:50:33 -0400

    Update MyndPalace.js

--- MyndPalace.js
+++ MyndPalace.js
@@ -9,8 +9,8 @@ import { conjurePaper } from 'psi/cantrip/paper.js'
 import 'psi/cantrip/introspection.js'
 
 avatar.self.dnaStore(import.meta.url);
-avatar.self.dnaStore('my/README.MD');
-avatar.self.dnaStore('my/LICENSE');
+avatar.self.dnaStore('./README.MD');
+avatar.self.dnaStore('./LICENSE');
 
 avatar.self.cantrips= cantrips;
 
