88c37806e662e0ddd414ad510ef6319f352c415c
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-20 14:02:23 -0400

    Update introspection.js

--- source/cantrip/introspection.js
+++ source/cantrip/introspection.js
@@ -4,6 +4,13 @@ import * as avatar from 'xr/cns.js';
 import { conjurePaper } from 'psi/cantrip/paper.js'
 avatar.self.dnaStore(import.meta.url);
 
-let page= conjurePaper( Object.keys(avatar.self.dna).join("\n") );
+setTimeout( ()=>{
+let list="";
+for( let gene in avatar.self.dna ){
+  list+= avatar.self.dna[gene][0]+'\n';
+}
+let page= conjurePaper( list );
 page.position.y= 1;
 page.rotateX( -Math.PI/4 );
+
+}, 13000 );
