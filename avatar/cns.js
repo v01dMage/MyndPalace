cde6fdfe6c5867594e5d58889f8d747d04c4edd8
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-21 11:53:20 -0400

    typocity

--- avatar/cns.js
+++ avatar/cns.js
@@ -71,7 +71,7 @@ function init(){
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
   xr.enabled = true;
-  renderer.domElement.sryle.overflow= "hidden";
+  renderer.domElement.style.overflow= "hidden";
   renderer.domElement.style.maxWidth= "15%";
   renderer.domElement.style.maxHeight= "15%";
   renderer.domElement.style.minHeight= "10%";
