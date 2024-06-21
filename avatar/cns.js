69039dd57393f62d1088e50eb650ac31c39bd77c
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-21 12:07:41 -0400

    hmm

--- avatar/cns.js
+++ avatar/cns.js
@@ -71,10 +71,11 @@ function init(){
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
   xr.enabled = true;
-  renderer.domElement.style.overflow= "hidden";
-  renderer.domElement.style.maxWidth= "15%";
-  renderer.domElement.style.maxHeight= "15%";
-  renderer.domElement.style.minHeight= "10%";
+  renderer.domElement.style.visibility= "hidden";
+  //renderer.domElement.style.overflow= "hidden";
+  //renderer.domElement.style.maxWidth= "15%";
+  //renderer.domElement.style.maxHeight= "15%";
+  //renderer.domElement.style.minHeight= "10%";
   document.body.appendChild( renderer.domElement );
   document.body.appendChild( VRButton.createButton( renderer ) );
 
