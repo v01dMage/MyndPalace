35094de859a3d5dd48fb2d1bf42217ca4db3463a
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-22 15:08:39 -0400

    touch visualizer

---
+++ source/parser/htmlTokenVisualizer.js
@@ -0,0 +1,6 @@
+// export a function who
+//  takes a token stream or array,then
+//  layers, places, and measures tokens,
+//  for a visual representation,
+// then, creates the representation in Three 
+//   dimensions, according to specs
