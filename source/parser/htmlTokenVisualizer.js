6f65001d8bbe46dc3e3fcc43fdb459999b33cf64
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-25 00:12:44 -0400

    visuals organizer

--- source/parser/htmlTokenVisualizer.js
+++ source/parser/htmlTokenVisualizer.js
@@ -1,6 +1,55 @@
 // export a function who
-//  takes a token stream or array,then
+//  takes a token stream array,then
 //  layers, places, and measures tokens,
 //  for a visual representation,
 // then, creates the representation in Three 
-//   dimensions, according to specs
+//   dimensions, according to specs,
+//   returning a Group of meshes.
+
+import * as THREE from 'three';
+
+export function visualizeHtml( tsa, specs ){
+  let layers= [];
+  let i= 0; //index
+  if( layers[i] == undefined ){
+    layers[i]= [];
+  }
+  let temp;
+
+  tsa.forEach( token=>{
+    if( token.type == "text" ){
+      layers[i].push( token )
+    } else if( token.type == "tag" ){
+      layers[i++].push( token );
+    } else { //type == endTag
+      do {
+        up:
+        i--;
+        if( i < 0 ){ //shouldn't get here..
+          i= 0;
+          layers[i].push( token );
+          break;
+        } else {
+        popping:
+          temp= layers[i].pop();
+          if( temp == undefined ){
+            layers.splice(i,1);
+            continue up;
+          }
+          if( temp.text == token.text ){
+            layers[i].push( temp );
+            layers[i].push( token );
+            break;
+          } else {
+            layers[i+1].shift( temp );
+            continue popping;
+          }
+        }
+      } while (true);
+    }
+  } );
+  // Now, with layers arranged
+  //   measurements and specs can be made
+
+
+}
