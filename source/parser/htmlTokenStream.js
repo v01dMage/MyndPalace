32e61bf1291ace041eda15320636b22e472a86e6
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-06-24 21:27:03 -0400

    continue tokenizer

--- source/parser/htmlTokenStream.js
+++ source/parser/htmlTokenStream.js
@@ -15,14 +15,33 @@ export function htmlTokenStream( html ){
   let result= [];
   let state= 0;
   for( let index= 0; index < chars.length; index++ ){
+    let char= chars(index)
     switch( state ){
       case 0: 
+        if( char == "<" ){
+          state= 1;
+        }
         break;
       case 1:
+        if( (/\w/).test(char) ){
+          letters.push(char);
+        } else if( (/\s/).test(char) ){
+          state= 2;
+        } else if( ">" == char ){
+          tag.push( letters.join('') );
+          letters= [];
+          state= 3;
+        }
         break;
       case 2: 
+        if( ">" == char ){
+          tag.push( letters.join('') );
+          letters= [];
+          state= 3;
+        }
         break;
       case 3:
+        
         break;
       case 4:
         break;
