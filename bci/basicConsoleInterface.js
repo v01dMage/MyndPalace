01c4e9cf2baeda9cd7ecca73d4392ebbf5bb4581
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-07-16 14:27:51 -0400

    continue buttons

--- bci/basicConsoleInterface.js
+++ bci/basicConsoleInterface.js
@@ -7,3 +7,22 @@ function getText(){
   return webText.value;
 }
 
+function makeLoadableButton(name, text){
+  let button= document.createElement('button');
+  button.innerHTML= name;
+  button.addEventListener('click', ()=>{
+    webText.value= text;
+  });
+  webButtons.appendChild(button);
+}
+
+function makeRunnableButton(name, fn){
+  //.
+}
+
+function makeButtons(o){
+  Object.keys(o).forEach( name=>{
+   if(typeof o[name] == 'string')
+    makeLoadableButton( name, o[name] );
+  } );
+}
