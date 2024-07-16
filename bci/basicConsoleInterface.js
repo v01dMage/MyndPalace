01403c1fda5c1a567d1b3497e99d8f9a153e2d76
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-07-16 14:32:08 -0400

    continue functions

--- bci/basicConsoleInterface.js
+++ bci/basicConsoleInterface.js
@@ -7,6 +7,25 @@ function getText(){
   return webText.value;
 }
 
+function clear(){
+    webOutput.innerHTML="";
+}
+
+function cout(html){
+  if(typeof html == null)
+    html= typeof html;
+  if(typeof html == undefined)
+    html= '<em>undefined</em>';
+  if(typeof html == 'object'){
+    html= Object.keys(html).join('<br>');
+    if(html=='') html= 'blank object';
+  }
+  let div= document.createElement('div');
+  div.style= 'display: inline; border: none;';
+  div.innerHTML= html;
+  webOutput.appendChild( div );
+}
+
 function makeLoadableButton(name, text){
   let button= document.createElement('button');
   button.innerHTML= name;
