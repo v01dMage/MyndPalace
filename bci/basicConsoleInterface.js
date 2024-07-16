a154eae1fa14bb651a3188acd40e3703d6eabbd2
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-07-16 14:17:52 -0400

    start bci

--- bci/basicConsoleInterface.js
+++ bci/basicConsoleInterface.js
@@ -1,1 +1,9 @@
 //basic console interface 
+const webOutput= document.getElementById('webOutput');
+const webText= document.getElementById('webText');
+const webButtons= document.getElementById('webButtons');
+
+function getText(){
+  return webText.value;
+}
+
