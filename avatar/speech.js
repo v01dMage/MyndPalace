2ab9690b4eb0d6222be13925e85d0772f044c939
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-07-29 12:18:07 -0400

    basic speak

--- avatar/speech.js
+++ avatar/speech.js
@@ -1,4 +1,10 @@
 //speech in js
+let cache= {};
+
 function speak(text){
-  
+  let utterance= cache[text] ? cache[text] :
+    cache[text]= new SpeechSynthesisUtterance( text );
+  window.speechSynthesis.speak( utterance );
 }
+
+export const speech= { speak };
