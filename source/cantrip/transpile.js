fd9802c33460ba43aad68595b64288f406552d2f
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-10-25 01:07:43 -0400

    should i be sleeping or testing this in system first 🥱🤔

--- source/cantrip/transpile.js
+++ source/cantrip/transpile.js
@@ -1,8 +1,9 @@
+//js test-option
 //export base transpile switch
 
 export function psiRun(text){
   //first line comment transpile options 
-  let re= /^(.*)[\r\n|\r|\n]/;
+  let re= /^\/\/([.\s]*)[\r\n|\r|\n]/;
   let opts= text.match( re );
 
   return opts;
