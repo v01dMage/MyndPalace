21b6022a9babeb405f6ad5aba475022246b3afe1
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-12-01 14:57:23 -0500

    inspectHtml ready to test.

--- source/cantrip/inspect.js
+++ source/cantrip/inspect.js
@@ -1,1 +1,14 @@
 //inspect an http address and visualize it
+
+import { htmlTokenStream } from 'parser/htmlTokenStream.js';
+import { visualizeHtml } from 'parser/htmlTokenVisualizer.js';
+import * as avatar from 'xr/cns.js';
+
+
+function inspectHtml( url ){
+  fetch(url).then( res=>res.text() ).then( txt=>{
+    let ts= htmlTokenStream( txt );
+    let target= visualizeHtml( ts );
+    avatar.self.scene.add( target );
+  });
+}
