cf9e25002d94c10961c716da0fea56a4e6f2f01e
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-12-01 15:06:13 -0500

    Update inspect test file manually

---
+++ tests/inspect.test.js
@@ -0,0 +1,10 @@
+//test inspect.js
+// test inspectHtml
+
+import { inspectHtml } from 'psi/inspect.js';
+
+inspectHtml( 'https://arretoken.com' );
+
+import * as avatar from 'xr/cns.js';
+
+avatar.self.console.ccout('👀');
