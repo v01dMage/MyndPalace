16ddf2fdf1b48d2985156616738f86871a236664
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2024-12-29 12:53:22 -0500

    Update logo3d.js base specs

--- source/parser/Logo/logo3d.js
+++ source/parser/Logo/logo3d.js
@@ -2,3 +2,14 @@
 // language specifications and 
 //  transpiler functions
 
+// traditional: fd, bk, rt, lt, pu, pd, 
+//              color 0xrgb
+//
+// 3d: line: capsule, +
+//     point: sphere, +
+//     rigid: point cube and line cylinder ?
+//     spin x,y,x hmmm
+//     xt, yt, zt +/-° (rt === yt)
+//    
+
+
