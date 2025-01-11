050a05609d64db8ba16b02b5a0b6a1b36dae69b3
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2025-01-10 23:46:32 -0500

    clean assignment and comments

--- source/cantrip/transpile.js
+++ source/cantrip/transpile.js
@@ -5,12 +5,10 @@ import { pout } from 'bci';
 
 export async function psiRun(text){
   //first line comment transpile options 
-  let rest;
-  let line= text.split('\n');
+  let [ line, ...rest ]= text.split('\n');
   if( typeof line != 'object' ){
     line= '//split failed to make object ';
   }else{
-    [ line, ...rest ]= line;
     if( typeof line != 'string' ){
       line= '//line failed string check';
     }
@@ -21,20 +19,13 @@ export async function psiRun(text){
   let base= opts.shift().substring(2);
 
   try{
-    // source/parser/${base}/pointer.txt
+
     let p= await fetch(`source/parser/${base}/pointer.txt`).then(res=>res.text());
     p= `parser/${base}/${p}`;
-    //base= 'pointer: '+p +'<br>';
     const { run }= await import(p);
-    let ran= await run( rest );
-    //base+= ran.test +'<br>';
-    //base+= ran.run +'<br>';
-    //base+= ran.t +'<br>';
+    run( rest );
+
   } catch(err){
     pout( err );
   }
-
-  //pout( '* '+ base );
-
-  //return true;
 }
