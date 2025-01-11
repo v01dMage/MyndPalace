a8085d87952f93ad199e723c5d9edced5b11e39d
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2025-01-10 23:57:42 -0500

    trust 🙄

--- source/cantrip/transpile.js
+++ source/cantrip/transpile.js
@@ -6,13 +6,7 @@ import { pout } from 'bci';
 export async function psiRun(text){
   //first line comment transpile options 
   let [ line, ...rest ]= text.split('\n');
-  if( typeof line != 'object' ){
-    line= '//split failed to make object ';
-  }else{
-    if( typeof line != 'string' ){
-      line= '//line failed string check';
-    }
-  }
+  
   let re= /^\/\/(.*)$/;
   let opts= line.match( re );
   opts= opts[0].split(' ');
