8c750152db59d7d706bdfddb5bcb71a6a12e06df
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2025-01-14 09:04:47 -0500

    rm comments

--- source/parser/Logo/logo3d.js
+++ source/parser/Logo/logo3d.js
@@ -70,8 +70,6 @@ class Turtle {
 
 }
 
-//var isDown= false;
-
 class Xyz {
   constructor( o={x:0,y:0,z:0} ){
     this.x = o.x;
@@ -84,11 +82,6 @@ class Xyz {
   }
 }
 
-//var position= new Xyz();
-//var rotation= new Xyz( position );
-
-//export var logo= { run, mv, pd, pu, fd };
-
 export async function run( t ){
   let logo= new Turtle();
   t.forEach( expression=>{ 
@@ -97,47 +90,5 @@ export async function run( t ){
     logo[cmd]( parts );
   });
 }
-/*
-function mv( arr ){
-  let [x,y,z]= arr;
-  position.x= x;
-  position.y= y;
-  position.z= z;
-}
 
-function pd(){
-  //pen down
-  if( !isDown ){
-    isDown= true;
-    pout( "isDown: "+ isDown );
-    //make sphere at position 
-    let sphere= new THREE.Mesh(
-      new THREE.SphereGeometry(),
-      new THREE.MeshBasicMaterial(
-        {color: 0x55cc77}
-      )
-    );
-    let p= position;
-    sphere.position.set( p.x, p.y, p.z );
-    avatar.self.scene.add( sphere );
-  }
-}
 
-function pu(){
-  //pen up
-  if( isDown ){
-    isDown= false;
-    pout( "isDown: "+ isDown );
-  }
-}
-
-function fd( d ){
-  // forward d-distance
-  // calculate new position from 
-  //    rotation and distance 
-  // create capsule geometry between 
-
-  let old= new Xyz( position );
-  pout( "pos: "+ old );
-  pout( "distance: "+ d );
-}*/
