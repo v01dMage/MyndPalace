dcb2e8c548b4b90e6f64fc2602c6f51979728d58
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2025-02-10 17:03:26 -0500

    whoops!! 🪵

--- source/parser/Logo/logo3d.js
+++ source/parser/Logo/logo3d.js
@@ -261,10 +261,10 @@ class Turtle {
 
   rotate( arr ){
     let axis= arr.shift();
-    pout( axis );
+    log( axis );
     let [rads, ms]= arr.map( Number.Float );
     let fn= this.latest['rotate'+axis];
-    pout(fn);
+    log(fn);
     return (o)=>{
       fn(rads*o.deltaTime/ms)
     };
