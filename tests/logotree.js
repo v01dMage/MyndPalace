656694d5750d2bcd24fc41bc4cac89975fd45510
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2025-03-13 16:41:05 -0400

    tweak

--- tests/logotree.js
+++ tests/logotree.js
@@ -2,6 +2,7 @@ import * as avatar from 'xr/cns.js';
 
 avatar.self.disc.position.set(0,-28,-40);
 avatar.self.psi(`//Logo noopts
+sz .15
 construct field
 setShape fd sphere
 rt 90
@@ -14,7 +15,7 @@ xt 6
 fd 6
 loop 6
 end
-construct reed
+construct reedG
 rt 95
 color random
 repeat 
@@ -27,6 +28,32 @@ fd 5.5
 loop 15
 bk 60
 fd 150
+color 0xdabb13
+sz .25
+repeat 
+xt 10 
+fd 10
+loop 18
+end
+construct reedW
+rt 95
+color random
+repeat 
+fd 30
+loop 4
+color 0xd47e30
+repeat
+xt 6
+fd 5.5
+loop 15
+bk 60
+fd 150
+color 0xffffff 
+sz .14
+repeat 
+xt 10 
+fd 10
+loop 18
 end
 pu
 mv 0 -30 -30
@@ -36,7 +63,28 @@ repeat
 fd 15
 nu field
 fd 15
-nu reed
+nu reedG
 rt 10
-loop 36
+loop 12 
+repeat
+fd 15
+nu field
+fd 15
+nu reedW
+rt 10
+loop 6
+repeat
+fd 15
+nu field
+fd 15
+nu reedG
+rt 10
+loop 12
+repeat
+fd 15
+nu field
+fd 15
+nu reedW
+rt 10
+loop 6
 `);
