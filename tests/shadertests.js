6058c1f825c90f6e41e991c83e81b157d694edcb
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2025-03-31 19:50:54 -0400

    partial boilerplate

--- tests/shadertests.js
+++ tests/shadertests.js
@@ -1,2 +1,12 @@
 // Load some buttons to play with shaders
+import * as avatar from 'xr/cns.js';
+import * as xrConsole from 'xr/console.js';
+
+const THREE= avatar.js3;
+
+const uniforms= {};
+
+const testGeometry= new THREE.BoxGeometry( 1,1,1 );
+const shader= new THREE.ShaderMaterial();
+
 
