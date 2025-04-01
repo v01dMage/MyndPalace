ccf16d03c51dac7726ca398cdcb660bf7eeb4137
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2025-04-01 03:07:17 -0400

    or that maybe

--- tests/shadertests.js
+++ tests/shadertests.js
@@ -34,7 +34,7 @@ void main(){
   vec4 result;
   pos = position;
   
-  result= vec4(position.x, sin(elapsedTime)+ position.y, position.z, 1.0);
+  result= vec4(position.x, sin(elapsedTime/1000.0)+ position.y, position.z, 1.0);
 
   gl_Position = projectionMatrix 
     * modelViewMatrix
