ec6f4ee9519ceeebcf1e4ec01a1306badb934e0c
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2025-04-02 23:49:13 -0400

    copy from test

--- assets/shaders/basicShaders.js
+++ assets/shaders/basicShaders.js
@@ -1,2 +1,59 @@
 //Basic shaders in THREE.ShaderMaterial format
 
+import { avatar } from 'mp';
+//import * as xrConsole from 'xr/console.js';
+//import { pout } from 'bci';
+
+const THREE= avatar.js3;
+
+const uniformsData= {
+  now: {
+    type: 'f',
+    value: Date.now()
+  }
+};
+
+let poll= ()=>{
+  pout( uniformsData.now.value );
+  setTimeout(poll, 5000);
+};
+//poll();  
+
+let tStart= Date.now();
+avatar.addToRecon(
+  (o)=>{
+    uniformsData.now.value= (o.now-tStart)/1000.0;
+    return o;
+  }
+);
+
+
+export const firstShader= new THREE.ShaderMaterial({
+  wireframe: true,
+  uniforms: uniformsData,
+  vertexShader: `uniform float now;
+varying vec3 pos;
+
+void main(){
+  vec4 result;
+  pos = position;
+  
+  result= vec4(position.x, sin(now)/3.0+ position.y , position.z, 1.0);
+
+  gl_Position = projectionMatrix 
+    * modelViewMatrix
+    * result;
+}
+`,
+  fragmentShader: `varying vec3 pos;
+uniform float now;
+
+void main(){
+  if( pos.x < 0.5 ){
+    gl_FragColor= vec4( (sin(now)/5.0)+0.5, 0.7, 0.0, 1.0);
+  } else {
+    gl_FragColor= vec4(0.8, 0.0, 1.0, 1.0);
+  }
+}
+`,
+});
