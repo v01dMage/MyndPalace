50a06f8e2e807d261336d889799e36a1bf3a64fe
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2025-04-04 17:31:10 -0400

    just in case....

--- assets/shaders/basicShaders.js
+++ assets/shaders/basicShaders.js
@@ -84,7 +84,7 @@ uniform float now;
 
 void main(){
   if( pos.y < 0 ){
-    gl_FragColor= vec4( (1*0.5 +0.2, 0.7, 0.0, 1.0);
+    gl_FragColor= vec4( (1.0*0.5 +0.2, 0.7, 0.0, 1.0);
   } else {
     gl_FragColor= vec4(0.8, 0.0, 1.0, 1.0);
   }
