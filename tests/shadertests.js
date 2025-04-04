33f9d786c050d3b41a4e4717b9f73a3a93a990bd
Talan Erickson <8935634+v01dMage@users.noreply.github.com>
2025-04-04 16:35:27 -0400

    Update shadertests.js to Cuboid

--- tests/shadertests.js
+++ tests/shadertests.js
@@ -2,13 +2,13 @@
 import { avatar } from 'mp';
 import * as xrConsole from 'xr/console.js';
 import { pout } from 'bci';
-import { firstShader } from 'inv/shaders/basicShaders.js';
+import { firstShader, Cuboid } from 'inv/shaders/basicShaders.js';
 
 const THREE= avatar.js3;
 
 const testGeometry= new THREE.BoxGeometry( 1,1,1, 4,4,4 );
 
-const mesh= new THREE.Mesh( testGeometry, firstShader );
+const mesh= new THREE.Mesh( testGeometry, Cuboid );
 mesh.position.set( 0, 1.6, -10);
 avatar.self.scene.add(mesh);
 
